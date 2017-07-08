3fdbe00ea3a272578ef1c64eb3737a4946ebd9a8
Don Cooper <don_cooper@hotmail.com>
2017-07-08 11:25:33 -0400

    Exercise 5 done

--- closure_exercises/exercise5.js
+++ closure_exercises/exercise5.js
@@ -3,9 +3,13 @@
 // This time you'll have to set up the flyTo variable yourself.
 
 var personToVisit = "best friend";
+
 // YOUR CODE HERE ------------------------
 
-
+function flyTo(place) {
+  var personToVisit = "best friend";
+  console.log('Enjoy your visit to ' + place + ' to visit your ' + personToVisit);
+}
 
 // ---------------------------------------
 
